010ffe4dfc09562e51723110d5f626b014ded879
Jane Park <9193jane@gmail.com>
2023-12-11 20:23:45 +0900

    20231211 RN-chat

---
+++ src/screens/Signin.js
@@ -0,0 +1,40 @@
+import React, { useContext } from 'react';
+import { ThemeContext } from "styled-components/native";
+import styled from 'styled-components/native';
+import {Button} from '../components';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+const Container = styled.View`
+  flex : 1;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({theme}) => theme.background};
+  padding: 0 20px;
+  padding-top: ${({insets : {top}}) => top}px;
+  padding-bottom: ${({insets : {bottom}}) => bottom}px;
+`;
+
+const StyledText = styled.Text`
+  font-size: 30px;
+  color: #111111;
+`;
+
+const Signin = ({navigation}) => {
+  const insets = useSafeAreaInsets();
+  const theme = useContext(ThemeContext);
+
+  return(
+    <Container insets={insets}>
+      <StyledText>로그인하기</StyledText>
+      <Button title='Sign in' onPress={() => console.log('Signin')} />
+      <Button 
+      title='or sign up' 
+      onPress={() => navigation.navigate('Signup')} 
+      containerStyle={{marginTop: 0, backgroundColor: 'transparent'}}
+      textStyle={{color: theme.btnTextLink, fontSize:18}}
+      />
+    </Container>
+  );
+};
+
+export default Signin;
---
+++ src/theme.js
@@ -0,0 +1,15 @@
+const Colors={
+  white: '#ffffff',
+  black: '#000000',
+  main: '#3679fe',
+};
+
+export const theme={
+  background: Colors.white,
+  Text: Colors.black,
+
+  // Button
+  btnBackground: Colors.main,
+  btnTitle: Colors.white,
+  btnTextLink: Colors.main,
+};
