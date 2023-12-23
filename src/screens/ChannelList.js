cf30197b8314d4ee74be186500073751353aac00
Jane Park <9193jane@gmail.com>
2023-12-23 20:21:47 +0900

    20231223 2-5 FlatList

--- src/screens/ChannelList.js
+++ src/screens/ChannelList.js
@@ -1,22 +1,89 @@
 import React from "react";
+import { FlatList } from "react-native";
 import styled from "styled-components/native";
 import { Button } from "../components";
+import { Ionicons } from "@expo/vector-icons";
+
+const channels = [];
+for (let i = 0; i < 1000; i++) {
+  channels.push({
+    id: i,
+    thtle: `title: ${i}`,
+    description: `desc: ${i}`,
+    createdAt: i,
+  });
+}
+
+const ItemContainer = styled.TouchableOpacity`
+  flex-direction: row;
+  align-items: center;
+  border-bottem-width: 1px;
+  border-color: ${({ theme }) => theme.itemBorder};
+  padding: 15px 20px;
+`;
+
+const ItemTextContainer = styled.View`
+  flex: 1;
+  flex-direction: column;
+`;
+
+const ItemTitle = styled.Text`
+  font-size: 20px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.text};
+`;
+
+const ItemDesc = styled.Text`
+  font-size: 16px;
+  margin-top: 5px;
+  color: ${({ theme }) => theme.ItemDesc};
+`;
+
+const ItemTime = styled.Text`
+  font-size: 12px;
+  color: ${({ theme }) => theme.itemTime};
+`;
+
+const ItemIcon = styled(Ionicons).attrs(({ theme }) => {
+  name: "chevron-forward";
+  size: 24;
+  color: theme.ItemIcon;
+})``;
+const Item = React.memo(
+  ({ item: { id, title, description, createdAt }, onPress }) => {
+  console.log(id);
+
+  return (
+    <ItemContainer>
+      <ItemTextContainer>
+        <ItemTitle>{title}</ItemTitle>
+        <ItemDesc>{description}</ItemDesc>
+      </ItemTextContainer>
+      <ItemTime>{createdAt}</ItemTime>
+      <ItemIcon />
+    </ItemContainer>
+  );
+});
 
 const Container = styled.View`
   flex: 1;
-  background-color: ${({theme}) => theme.background};
+  background-color: ${({ theme }) => theme.background};
 `;
 const StyledText = styled.Text`
   font-size: 30px;
 `;
 
-const ChannelList = ({navigation}) => {
+const ChannelList = ({ navigation }) => {
   return (
     <Container>
-      <StyledText>Channel List</StyledText>
-      <Button title="Chennel" onPress={() => navigation.navigate('Chennel')} />
+      <FlatList
+        data={channels}
+        renderItem={({ item }) => <Item item={item} />}
+        keyExtractor={(item) => item["id"].toString()}
+        windowSize={10}
+      />
     </Container>
-  )
-}
+  );
+};
 
 export default ChannelList;
--- src/theme.js
+++ src/theme.js
@@ -1,14 +1,16 @@
 const Colors={
   white: '#ffffff',
   black: '#000000',
-  main: '#3679fe',
+  main: '#4C4637',
   grey_0: '#d5d5d5',
   grey_1: '#a6a6a6',
-  red: '#c00'
+  red: '#F99053',
+  bgColor: '#FFF8E7',
+  transparent: 'transparent',
 };
 
 export const theme={
-  background: Colors.white,
+  background: Colors.bgColor,
   text: Colors.black,
   errorText: Colors.red,
 
@@ -19,12 +21,12 @@ export const theme={
   btnSignout: Colors.red,
 
   // Image
-  imgBackground: Colors.white,
+  imgBackground: Colors.transparent,
   imgBtnBackground: Colors.grey_1,
   imgBtnIcon: Colors.white,
 
   // Input
-  inputBackground: Colors.white,
+  inputBackground: Colors.transparent,
   inputLabel: Colors.grey_1,
   inputPlaceholder: Colors.grey_1,
   inputBorder:Colors.grey_1,
@@ -37,4 +39,10 @@ export const theme={
   //Tab
   tabBtnActive: Colors.main,
   tabBtnInactive: Colors.grey_1,
+
+  //List-Item
+  itemBorder: Colors.grey_0,
+  itemTime:Colors.grey_1,
+  itemDesc:Colors.grey_1,
+  itemIcon:Colors.grey_1
 };
